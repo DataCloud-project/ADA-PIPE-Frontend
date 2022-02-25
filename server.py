ce929e7e66f21ec83946cc90ff301dca95d089bd
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-02-25 21:31:20 +0100

    Changed the output of a restcall to only include the values of the outgoing template

--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
 # source: https://towardsdatascience.com/a-python-api-for-background-requests-based-on-flask-and-multi-processing-187d0e3049c9
-from cgitb import html
-from pickle import GET
-from turtle import pd
 from flask import Flask, render_template, Response
 from waitress import serve
 from flask_cors import CORS
@@ -40,12 +37,17 @@ def get_jobs():
     jobs = {'job': [job._job_dict for job in pdc.get_jobs()]}
     return Response(json.dumps(jobs), mimetype='application/json')
 
+@app.route('/job/requirement')
+def get_req():
+    job_requirement = json.dumps(pdc.get_jobs().pop().get_requirements())
+    return Response(job_requirement, mimetype='application/json')
+
 
 @app.route('/pipelines', methods=[REST_GET])
 def get_pipelines():
 
     pipelines = {'pipelines': [
-        pdc.get_json_dict(), pdc2.get_json_dict()]}
+        pdc.get_outgoing_dict(), pdc2.get_outgoing_dict()]}
 
     return Response(json.dumps(pipelines), mimetype='application/json')
 
@@ -53,6 +55,9 @@ def get_pipelines():
 @app.route('/pipeline_names', methods=[REST_GET])
 def get_pipeline_names():
     return {'pipelines': [pdc.get_pipeline_name(), pdc2.get_pipeline_name()]}
+
+
+
 
 
 if __name__ == '__main__':
