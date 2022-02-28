7ab5fd5d183842451d8d439ac8610c29bace72ae
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-02-28 12:49:30 +0100

    Added functions to pipeline_state.py

--- pipeline_state.py
+++ pipeline_state.py
@@ -8,8 +8,15 @@ class PipelineState():
     def __init__(self) -> None:
         self.__pipelines: List[PipelineDataContainer] = []
 
-    def add_pipeline(self, pipeline: PipelineDataContainer):
+    def add_pipeline_container(self, pipeline: PipelineDataContainer):
         self.__pipelines.append(pipeline)
 
     def get_pipelines(self) -> List[PipelineDataContainer]:
         return deepcopy(self.get_pipelines)
+
+    def get_pipelines_(self) -> List[PipelineDataContainer]:
+        return self.__pipelines
+
+    def __get__(self, idx) -> PipelineDataContainer:
+        return self.__pipelines[idx]
+
